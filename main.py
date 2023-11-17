f49b94d9424fcaca9772cc56c47037ba5c712a69
Yuan <chengxuyuan1998@gmail.com>
2023-11-17 21:38:15 +0000

    implement decoder

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import yaml
-from models.encoder import Encoder
 import torchvision.transforms as transforms
+from models.encoder import Encoder
+from models.decoder import Decoder
 from data.FlickrDataLoader import get_loader
 
 with open('./configs/config.yaml', 'r') as config_file:
@@ -19,10 +20,13 @@ image_transform = transforms.Compose([
 if __name__ == '__main__':
     loader, dataset = get_loader(images_path, caption_path, image_transform, batch_size)
     encoder = Encoder()
+    decoder = Decoder(dataset.vocabulary.__len__(), 300, 10)
     encoder.eval()
+    decoder.eval()
     for (imgs, captions, seq_len) in loader:
         print(imgs.shape)
         print(captions.shape)
         print(seq_len)
         print(encoder(imgs).shape)
+        print(decoder(captions))
         break
