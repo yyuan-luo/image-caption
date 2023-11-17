2ddf5ba5d9c33d7ba406952cd3067ca85e3ea57c
Yuan <chengxuyuan1998@gmail.com>
2023-11-17 21:37:35 +0000

    tokenize the label

--- dataSet.py
+++ dataSet.py
@@ -1,16 +1,22 @@
 import torch
 import os
+import nltk
 from os import walk
 from PIL import Image
 from torch.utils.data import Dataset
+
 
 def load_captions(path):
     f = open(path, "r")
     f.readline()
     captions = {}
     for line in f:
-        key, value = line.split(",", 1)
-        captions[key] = value
+        image, caption = line.split(",", 1)
+        caption = caption.replace('"', '')
+        caption_tokens = nltk.word_tokenize(caption)
+        caption_tokens.insert(0, '[SOS]')
+        caption_tokens.append('[EOS]')
+        captions[image] = caption_tokens
     f.close()
     return captions
 
@@ -24,24 +30,12 @@ def load_image(path):
     image = Image.open(path)
     return image
 
-def get_max_width_height(folder_path, image_names):
-    max_width, max_height = 0, 0
-    for file in image_names:
-        image = Image.open(folder_path + "/" + file)
-        width, height = image.size
-        if width > max_width:
-            max_width = width
-        if height > max_height:
-            max_height = height
-    return max_width, max_height
-
 
 class ImageCaptionDataset(Dataset):
     def __init__(self, image_foler_path, caption_path, transform=None):
         self.image_folder_path = image_foler_path
         self.image_names = get_image_names(image_foler_path)
         self.captions = load_captions(caption_path)
-        self.max_width, self.max_height = get_max_width_height(self.image_folder_path, self.image_names)
         self.transform = transform
 
     def __len__(self):
--- main.py
+++ main.py
@@ -1,8 +1,45 @@
 from dataSet import ImageCaptionDataset
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+
+
+def calculate_padding(image, target_size):
+    width, height = image.size
+    target_width, target_height = target_size
+
+    left_padding = max(0, (target_width - width) // 2)
+    right_padding = max(0, target_width - width - left_padding)
+    top_padding = max(0, (target_height - height) // 2)
+    bottom_padding = max(0, target_height - height - top_padding)
+
+    return left_padding, top_padding, right_padding, bottom_padding
+
+
+class DynamicPadding:
+    def __init__(self, target_size):
+        self.target_size = target_size
+
+    def __call__(self, image):
+        padding = calculate_padding(image, self.target_size)
+        padding_transforms = transforms.Pad(padding, fill=0)
+        return padding_transforms(image)
+
 
 folder_path = "./data/flickr8k/images"
 caption_path = "./data/flickr8k/captions.txt"
+target_size = (500, 500)
 
-dataset = ImageCaptionDataset(folder_path, caption_path)
+image_transform = transforms.Compose([
+    DynamicPadding(target_size),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
 
-print(dataset.max_width, dataset.max_height)
+if __name__ == '__main__':
+    dataset = ImageCaptionDataset(folder_path, caption_path, image_transform)
+
+    batch_size = 32
+    data_loader = DataLoader(dataset, batch_size, shuffle=True, collate_fn=lambda x: x)
+
+    for batch in data_loader:
+        print(batch[0])
