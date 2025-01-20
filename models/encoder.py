d31127e51fa6bc9eea331a0197a0edb86278363e
Yuan Luo <chengxuyuan1998@gmail.com>
2025-01-20 09:50:07 +0100

    partially frozen resnet50

--- models/encoder.py
+++ models/encoder.py
@@ -10,6 +10,10 @@ class Encoder(nn.Module):
         # resnet50 as backbone
         self.resnet50 = models.resnet50(weights=ResNet50_Weights.IMAGENET1K_V1)
         self.resnet50.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        for param in self.resnet50.parameters():
+            param.requires_grad = False
+        for param in self.resnet50.layer4.parameters():
+            param.requires_grad = True
         modules = list(self.resnet50.children())[:-1]
         self.resnet50 = nn.Sequential(*modules)
         self.embedding = nn.Linear(2048, embedding_size)    # TODO: replace 2048 with a variable
