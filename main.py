fix loss calculation

main.py
@@ -72,8 +72,7 @@ if __name__ == '__main__':
         model.train()
         train_loss_epoch = []
         train_loss = []
-        params = list(model.embedding.parameters())
-        optimizer = torch.optim.Adam(params, lr)
+        optimizer = torch.optim.Adam(model.parameters(), lr)
         outer = tqdm(total=num_epochs - training_starting_file, desc="Epoch", position=0, leave=True)
         train_log = tqdm(total=0, position=2, bar_format='{desc}', leave=False)
         val_best = 100
@@ -84,10 +83,8 @@ if __name__ == '__main__':
                 imgs = imgs.to(device)
                 captions = captions.to(device)
                 seq_lens = torch.tensor(seq_lens).to(device)
-                output = model(imgs, captions[:, :-1], seq_lens)
-                captions = captions.reshape(-1)
-                loss = criterion(torch.reshape(output, (-1, )), captions)  # Compute loss
-
+                output = model(imgs, captions, seq_lens)
+                loss = criterion(output.view(-1, len(dataset.vocabulary)), captions.view(-1))  # Compute loss
                 loss.backward()
                 optimizer.step()
                 L = loss.item()
@@ -103,8 +100,7 @@ if __name__ == '__main__':
                 captions = captions.to(device)
                 seq_lens = torch.tensor(seq_lens).to(device)
                 output = model(imgs, captions, seq_lens)
-                captions = torch.reshape(captions, (-1,))
-                loss = criterion(output, captions)
+                loss = criterion(output.view(-1, len(dataset.vocabulary)), captions.view(-1))
                 L = loss.item()
                 val_loss.append(L)
             average_loss = sum(val_loss) / len(val_loss)
@@ -131,8 +127,7 @@ if __name__ == '__main__':
             captions = captions.to(device)
             seq_lens = torch.tensor(seq_lens).to(device)
             output = model(imgs, captions, seq_lens)
-            captions = torch.reshape(captions, (-1,))
-            loss = criterion(output, captions)
+            loss = criterion(output.view(-1, len(dataset.vocabulary)), captions.view(-1))
             L = loss.item()
             test_loss.append(L)
             stats = 'Loss: %.4f, Perplexity: %5.4f' % (L, np.exp(L))

models/res2rnn.py
@@ -1,8 +1,8 @@
 import torch
 from torch import nn
 
-from encoder import Encoder
-from decoder import Decoder
+from models.encoder import Encoder
+from models.decoder import Decoder
 
 
 class Res2RNN(nn.Module):
@@ -19,10 +19,12 @@ class Res2RNN(nn.Module):
 
 if __name__ == '__main__':
     embedding_size = 256
-    images_feature = torch.randn(3, 3, 224, 224)
+    images = torch.randn(3, 3, 224, 224)
     captions = torch.randint(1, 10, [3, 20])
-    seq_lengths = torch.tensor([1, 1, 3], dtype=torch.int)
+    seq_lengths = torch.tensor([10, 12, 20], dtype=torch.int)
+    print(images.shape, captions.shape, seq_lengths.shape)
     model = Res2RNN(vocabulary_size=100, embedding_size=embedding_size)
-    output = model(images_feature, captions[:, :-1], seq_lengths)
-    print(output.shape)
-    print(torch.reshape(output, (-1, )).shape)
+    criterion = nn.CrossEntropyLoss(ignore_index=0)  # ignore <PAD>
+    output = model(images, captions, seq_lengths)
+    print(output.view(-1, 100).shape, captions.view(-1).shape)
+    print(criterion(output.view(-1, 100), captions.view(-1)))